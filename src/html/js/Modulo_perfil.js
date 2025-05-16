d96e20b526c70d680820541ee4babe031911f8fc
Raul Sanabria Rodado <rsanrod1@epsg.upv.es>
2025-05-16 02:00:20 +0200

    modulo perfil acabado

---
+++ src/html/js/Modulo_perfil.js
@@ -0,0 +1,12 @@
+
+//alert de el cerrar sesion
+document.addEventListener("DOMContentLoaded", () => {
+    const cerrarSesion = document.getElementById("cerrarSesion");
+
+
+    cerrarSesion.addEventListener("click", () => {
+        alert("La sesión ha sido cerrada")
+    })
+
+
+})
