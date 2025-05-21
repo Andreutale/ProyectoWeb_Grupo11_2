37bba76196aee14f3ab1cace71fa2dba83fcb7d2
ferran <ferransansaloni@gmail.com>
2025-05-21 18:49:27 +0200

    Funcionan muchas cosasssss

---
+++ src/html/php/Web_inicio_sesion.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include('../../app/conexion.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $correo = $_POST['correo'];
+    $contraseña = $_POST['contraseña'];
+
+    $stmt = $conexion->prepare("SELECT id, contraseña, token FROM usuariosWeb WHERE correo = ?");
+    $stmt->bind_param("s", $correo);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows === 1) {
+        $stmt->bind_result($id, $hash_guardado, $token);
+        $stmt->fetch();
+
+        if (password_verify($contraseña, $hash_guardado)) {
+            $_SESSION['token'] = $token;
+            header("Location: ../modulo/Modulo_inicio_sesion.php");
+            exit();
+        } else {
+            header("Location: ../web/Web_inicio_sesion.html?error=1");
+            exit();
+        }
+    } else {
+        header("Location: ../web/Web_inicio_sesion.html?error=1");
+        exit();
+    }
+
+    $stmt->close();
+    $conexion->close();
+}
+?>
