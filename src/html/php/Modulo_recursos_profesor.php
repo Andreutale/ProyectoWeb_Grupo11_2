62f4c61eb48ce310d73e542427eac92fa48b8954
Andreutale <amandreu3@gmail.com>
2025-05-29 21:12:03 +0200

    bases de datos modulo terminadas
    falta retocar cosas

--- src/html/php/Modulo_recursos_profesor.php
+++ src/html/php/Modulo_recursos_profesor.php
@@ -5,12 +5,12 @@ include('../../app/conexion.inc');
     $id_autor = 1;
     $id_asignatura = 1;
     // $nombre = $_FILES['archivo']['name']; nombre original del fichero
-    $nombre = $_FILES['nombre']; // nombre personalizado
+    $nombre = $_POST['nombre']; // nombre personalizado
     $tipo = $_FILES['archivo']['type'];
     $archivo = file_get_contents($_FILES['archivo']['tmp_name']);
 
     $stmt = $conexion->prepare("INSERT INTO archivosModulo (id_autor, id_asignatura, nombre, tipo, datos) VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param("issss", $id_autor, $id_asignatura, $nombre, $tipo, $archivo);
+    $stmt->bind_param("iisss", $id_autor, $id_asignatura, $nombre, $tipo, $archivo);
 
     if ($stmt->execute()) {
         // Archivo subido exitoso
